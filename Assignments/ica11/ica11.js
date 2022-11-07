b41ca6858cedb6dc4746d712f671cd622c1e943b
bencunningham2019 <bencunningham2019@gmail.com>
2022-11-06 22:47:09 -0700

    ica11

--- Assignments/ica11/ica11.js
+++ Assignments/ica11/ica11.js
@@ -3,28 +3,80 @@
 const quoteButton = document.querySelector('.new-quote');
 quoteButton.addEventListener('click', getQuote);
 
-let trump = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random'
+const deliveryButton = document.querySelector('.new-delivery');
+deliveryButton.addEventListener('click', getDelivery);
 
+let joke = 'https://v2.jokeapi.dev/joke/Any?safe-mode'
+
+i
 async function getQuote() {
     //console.log('test click worked');
-    let text = await fetch(trump)
+    let text = await fetch(joke)
     let response = await text.text();
     
     let json_response = JSON.parse(response);
-    console.log(json_response['message'])
+    console.log(json_response['setup'])
     // .then(text => text.text());
     // console.log(text['message']);
     
-    displayQuote(json_response['message'])
+    displayQuote(json_response['setup']) 
+}
+
+async function getDelivery() {
+    //console.log('test click worked');
+    let text = await fetch(joke)
+    let response = await text.text();
+    
+    let json_response = JSON.parse(response);
+    console.log(json_response['delivery'])
+    // .then(text => text.text());
+    // console.log(text['message']);
+    
+    displayDelivery(json_response['delivery'])
 }
 
 function displayQuote(x) {
     const quoteBox = document.querySelector('#js-quote-text');
     // console.log('display quote working');
-    //let textMessage = document.createTextNode(x);
+    let textMessage = document.createTextNode(x);
 
-    //quoteBox.appendChild(textMesssage);
+    // quoteBox.appendChild(textMesssage);
     document.getElementById('js-quote-text').textContent = x;
 }
 
+function displayDelivery(x) {
+    const quoteBox = document.querySelector('#js-delivery-text');
+    // console.log('display quote working');
+    let textMessage = document.createTextNode(x);
+
+    // quoteBox.appendChild(textMesssage);
+    document.getElementById('js-delivery-text').textContent = x;
+}
+
 getQuote();
+
+// const jokeButton = document.querySelector('.new-joke');
+// jokeButton.addEventListener('click', getJoke);
+
+// let joke = 'https://v2.jokeapi.dev/joke/Any';
+
+// async function getJoke() { 
+//     let response = await fetch(joke);
+//     let data = await response.text();
+
+//     let json_response =JSON.parse(data);
+//     console.log(json_response['setup'])
+
+//     displayJoke(json_response['message'])
+// };
+
+// // getJoke().then(data => console.log(data));
+ 
+// function displayJoke(x) {
+//     const jokeBox = document.querySelector('#js-joke-text');
+//     // console.log('display quote working');
+//     let textMessage = document.createTextNode(x);
+
+//     // quoteBox.appendChild(textMesssage);
+//     document.getElementById('js-joke-text').textContent = x;
+// }
