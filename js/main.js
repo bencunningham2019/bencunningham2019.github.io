3bd8dffd0c9de5ac532a16e1371c523225bd8442
bencunningham2019 <bencunningham2019@gmail.com>
2022-12-06 09:40:31 -0700

    Updates

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// let img = document.getElementsByClassName("one");
+// let img = document.getElementById("beans");
+
+// function enlargeImg() {
+//     img.style.transform = 'scale(1.5)';
+//     img.style.transition = "transform 0.25s ease";
+//     img.style.display = "inline-block"
+// }
+
+// document.getElementsByClassName
+
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('img')
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active')
+    const img = document.createElement('img')
+    img.src = image.src
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(img)
+  })
+})
+
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+  lightbox.classList.remove('active')
+})
